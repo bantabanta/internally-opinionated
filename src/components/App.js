c388483fef903f709b9be256f6f48ec353177cf5
Tim Banta <timbanta@me.com>
2022-11-08 21:59:07 -0800

    feat: add actions, reducers, middleware logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,10 @@
-function App() {
+import { useEffect } from "react";
+import { handleInitialData } from "../actions/shared";
+
+function App(props) {
+  // useEffect(() => {
+  //   props.dispatch(handleInitialData());
+  // }, []);
   return <div>Hello World</div>;
 }
 
---
+++ src/reducers/questions.js
@@ -0,0 +1,13 @@
+import { RECEIVE_QUESTIONS } from "../actions/questions";
+
+export default function users(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_QUESTIONS:
+      return {
+        ...state,
+        ...action.questions,
+      };
+    default:
+      return state;
+  }
+}
